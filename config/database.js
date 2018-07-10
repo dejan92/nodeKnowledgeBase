12dbe9f3e697071a6b326b09ff1c9ea430cefc99
djovanov92 <djovanov92@gmail.com>
2018-07-10 16:58:42 +0200

    Register User working

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
     //database: 'mongodb://localhost:27017/nodekb',
     database: "mongodb://localhost:27017/nodekb",
     secret: 'YourSecret'
--- routes/users.js
+++ routes/users.js
@@ -11,64 +11,64 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
-// //Register Process
-// router.post('/register', (req, res) => {
-//     const name = req.body.name;
-//     const email = req.body.email;
-//     const username = req.body.username;
-//     const password = req.body.password;
-//     const password2 = req.body.password2;
+//Register Process
+router.post('/register', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+    const password2 = req.body.password2;
 
-//     req.checkBody('name', 'Name is required').notEmpty();
-//     req.checkBody('email', 'Email is required').notEmpty();
-//     req.checkBody('email', 'Email is not valid').isEmail();
-//     req.checkBody('username', 'Username is required').notEmpty();
-//     req.checkBody('password', 'Password is required').notEmpty();
-//     req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 
-//     let errors = req.validationErrors();
+    let errors = req.validationErrors();
 
-//     if (errors) {
-//         res.render('register', {
-//             errors
-//         });
-//     } else {
-//         let newUser = new User({
-//             name,
-//             email,
-//             username,
-//             password
-//         });
+    if (errors) {
+        res.render('register', {
+            errors
+        });
+    } else {
+        let newUser = new User({
+            name,
+            email,
+            username,
+            password
+        });
 
-//         bcrypt.genSalt(10, (err, salt) => {
-//             bcrypt.hash(newUser.password, salt, (err, hash) => {
-//                 if (err) return console.log(err);
-//                 newUser.password = hash;
-//                 newUser.save((err) => {
-//                     if (err) return console.log(err);
-//                     else {
-//                         req.flash('success', 'You are now register, please log in');
-//                         res.redirect('/users/login');
-//                     }
-//                 });
-//             })
-//         });
-//     }
-// });
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) return console.log(err);
+                newUser.password = hash;
+                newUser.save((err) => {
+                    if (err) return console.log(err);
+                    else {
+                        req.flash('success', 'You are now register, please log in');
+                        res.redirect('/users/login');
+                    }
+                });
+            })
+        });
+    }
+});
 
 
-// //Login Form
-// router.get('/login', (req,res)=>{
-//     res.render('login');
-// });
+//Login Form
+router.get('/login', (req,res)=>{
+    res.render('login');
+});
 
-// //Login Process
-// router.post('/login', (req,res,next)=>{
-//     passport.authenticate(local, {
-//         successRedirect:'/',
-//         failureRedirect:'/users/login',
-//         failureFlash: true
-//     })(req,res,next);
-// });
+//Login Process
+router.post('/login', (req,res,next)=>{
+    passport.authenticate(local, {
+        successRedirect:'/',
+        failureRedirect:'/users/login',
+        failureFlash: true
+    })(req,res,next);
+});
 
 module.exports = router;
